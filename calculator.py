abb9c3170f6993295ef4e702fbec5e96373ecaa2
RaymartBartilet <114345498+Raymartbartilet@users.noreply.github.com>
2022-10-21 21:53:25 +0800

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,22 @@
+name = input("\n Enter your Name: ")
+print ("\n Hello " + name + "! the answers are, "'\n')
+
+# Python program to perform Addition Subtraction Multiplication
+# and Division of two numbers
+
+Student_count1= 2
+Student_count2= 50
+
+add = Student_count1+Student_count2
+print(add)
+
+sub = Student_count1-Student_count2
+print(sub)
+
+mul = Student_count1*Student_count2
+print(mul)
+
+div = Student_count1/Student_count2
+print(div)
+ 
+ #end program for student count..
